aa24d1d193eb27a0d9adf7abbf4b9e9928aa5367
Benjamin Shih <benjaminshh@gmail.com>
2020-08-22 11:09:01 -0700

    edited a mistake

--- cachematrix.R
+++ cachematrix.R
@@ -24,10 +24,10 @@ cacheSolve <- function(x, ...) {
   i <- x$getinverse()
   if(!is.null(i)){
     message("getting cached data")
-    return(m)
+    return(i)
   }
   data <- x$get()
-  i <- solve(x,...)
+  i <- solve(data,...)
   x$setinverse(i)
   i
 }
